888525e08930fd1be3b4848e869f6e2f53ab22f8
Joel Manasseh <joel_manasseh@brown.edu>
2023-01-23 15:55:06 -0500

    update model.py

--- code/model.py
+++ code/model.py
@@ -1,31 +1,35 @@
 import numpy as np
 from sklearn import svm
 
+class InvalidLabelException(Exception):
+        "Raised when the expected number of labels is incorrect"
+        pass
 
 class SVM(object):
 
     def __init__(self):
         self.model = svm.LinearSVC()
+
     
     def train(self, inputs, labels):
         """
+        DO NOT MODIFY THIS FUNCTION
         This function trains the binary SVM classifier based on the inputs and corresponding labels
 
         Inputs:
-        inputs: a n x d numpy matrix where n is the number of samples and d is the number of features
-        labels: a 1 x n numpy array where n is the number of samples
+        inputs: a nxd numpy matrix where n is the number of samples and d is the number of features
+        labels: a 1xn numpy array where n is the number of samples
 
         Outputs:
-        A 1 x d numpy array representing the weights from the model where d is the number is the number of features,
-        and a number representing the bias from the model
+        none
         """
         try:
             if (np.size(np.unique(labels)) > 2):
-                raise Exception
+                raise InvalidLabelException
             self.model.fit(inputs, labels)
             weights = self.model.coef_
             bias = self.model.intercept_
             return weights, bias
-        except Exception:
-            print("More than 2 labels have been inputted, function only accepts 2 labels")
+        except InvalidLabelException:
+            print("Exception occurred: Inputted more than 2 unique labels, only accepts 2 unique labels")
             raise
